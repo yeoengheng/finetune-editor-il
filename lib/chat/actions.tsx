430a7bf6ad2368c124acba9725488e128ebf49d6
Eng Heng <enghengyeo12@Hotmail.com>
2024-04-26 18:32:56 +0800

    katie dover

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -142,27 +142,14 @@ async function submitUserMessage(content: string) {
   let textNode: undefined | React.ReactNode
 
   const ui = render({
-    model: 'gpt-3.5-turbo',
+    model: 'ft:gpt-3.5-turbo-0125:kabana:katie-dover:9DSe2QJM',
     provider: openai,
     initial: <SpinnerMessage />,
     messages: [
       {
         role: 'system',
-        content: `\
-You are a stock trading conversation bot and you can help users buy stocks, step by step.
-You and the user can discuss stock prices and the user can adjust the amount of stocks they want to buy, or place an order, in the UI.
-
-Messages inside [] means that it's a UI element or a user event. For example:
-- "[Price of AAPL = 100]" means that an interface of the stock price of AAPL is shown to the user.
-- "[User has changed the amount of AAPL to 10]" means that the user has changed the amount of AAPL to 10 in the UI.
-
-If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
-If the user just wants the price, call \`show_stock_price\` to show the price.
-If you want to show trending stocks, call \`list_stocks\`.
-If you want to show events, call \`get_events\`.
-If the user wants to sell stock, or complete another impossible task, respond that you are a demo and cannot do that.
-
-Besides that, you can also chat with users and do some calculations if needed.`
+        content: `You are a LinkedIn post writer for Irrational Labs. You write in the voice, style, tone and structure of Katie Dover. 
+        You strictly only use the content provided in the user prompt and nothing else. Keep the meaning and factuality of the information given to you. Don't add. Use the hook to start. Make it flow with the key points and complete it with the CTA.`
       },
       ...aiState.get().messages.map((message: any) => ({
         role: message.role,
